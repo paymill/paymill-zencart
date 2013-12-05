fb140e4b9362abc3dc2fbbcd39b6d03db23f94bc
Thilo Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-12-05 15:25:09 +0100

    PMZC-14 Tweaked the fast checkout calls in the payment controlles to use the refactored class

--- includes/modules/payment/paymillElv.php
+++ includes/modules/payment/paymillElv.php
@@ -82,7 +82,7 @@ class paymillElv extends paymill_abstract
                 . 'var paymill_elv_iban = "' . utf8_decode($payment['iban']) . '";'
                 . 'var paymill_elv_bic = "' . utf8_decode($payment['bic']) . '";'
                 . 'var paymill_elv_account = "' . $payment['account'] . '";'
-                . 'var paymill_elv_fastcheckout = ' . $this->fastCheckout->canCustomerFastCheckoutElvTemplate($_SESSION['customer_id']) . ';'
+                . 'var paymill_elv_fastcheckout = ' . $this->fastCheckout->canCustomerFastCheckoutElv($_SESSION['customer_id']) . ';'
                 . 'var checkout_payment_link = "' . zen_href_link(FILENAME_CHECKOUT_PAYMENT, 'step=step2', 'SSL', true, false). '&payment_error=' . $this->code . '&error=' . '";'
                 . '</script>'
                 . '<script type="text/javascript" src="ext/modules/payment/paymill/public/javascript/elv.js"></script>';
