f56ec1e74b649a262c59d01b5712e38d67152555
Thilo Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-12-09 16:33:42 +0100

    PIB-49 Added user validation to the fast checkout process.

--- ext/modules/payment/paymill/FastCheckout.php
+++ ext/modules/payment/paymill/FastCheckout.php
@@ -7,20 +7,22 @@ class FastCheckout
     public function canCustomerFastCheckoutCcTemplate($userId)
     {
         $flag = 'false';
-        if ($this->canCustomerFastCheckoutCc($userId)) {
-            $flag = 'true';
+        if(isset($userId) && $userId != ''){
+            if ($this->canCustomerFastCheckoutCc($userId)) {
+                $flag = 'true';
+            }
         }
-        
         return $flag;
     }    
     
     public function canCustomerFastCheckoutElvTemplate($userId)
     {
         $flag = 'false';
-        if ($this->canCustomerFastCheckoutElv($userId)) {
-            $flag = 'true';
+        if(isset($userId) && $userId != ''){
+            if ($this->canCustomerFastCheckoutElv($userId)) {
+                $flag = 'true';
+            }
         }
-        
         return $flag;
     }    
     
@@ -49,13 +51,14 @@ class FastCheckout
     public function saveElvIds($userId, $newClientId, $newPaymentId)
     {   
         global $db;
-        if ($this->_canUpdate($userId)) {
-            $sql = "UPDATE `". DB_PREFIX . "pi_paymill_fastcheckout`SET `paymentID_ELV` = '$newPaymentId' WHERE `userID` = '$userId'";
-        } else {
-            $sql = "INSERT INTO `". DB_PREFIX . "pi_paymill_fastcheckout` (`userID`, `clientID`, `paymentID_ELV`) VALUES ('$userId', '$newClientId', '$newPaymentId')";
+        if(isset($userId) && $userId != ''){
+            if ($this->_canUpdate($userId)) {
+                $sql = "UPDATE `". DB_PREFIX . "pi_paymill_fastcheckout`SET `paymentID_ELV` = '$newPaymentId' WHERE `userID` = '$userId'";
+            } else {
+                $sql = "INSERT INTO `". DB_PREFIX . "pi_paymill_fastcheckout` (`userID`, `clientID`, `paymentID_ELV`) VALUES ('$userId', '$newClientId', '$newPaymentId')";
+            }
         }
-        
-       $db->Execute($sql);
+        $db->Execute($sql);
     }
     
     private function _canUpdate($userId)
@@ -68,23 +71,30 @@ class FastCheckout
     {
         global $db;
         $sql = "SELECT * FROM `". DB_PREFIX . "pi_paymill_fastcheckout` WHERE `userID` = '$userId'";
-        
+
         $fastCheckout = $db->Execute($sql);
-        
+
         return $fastCheckout->fields;
     }
     
     public function hasElvPaymentId($userId)
     {
-        $data = $this->loadFastCheckoutData($userId);
-        return $data && array_key_exists('paymentID_ELV', $data) && !empty($data['paymentID_ELV']);
+        if(isset($userId) && $userId != ''){
+            $data = $this->loadFastCheckoutData($userId);
+            return $data && array_key_exists('paymentID_ELV', $data) && !empty($data['paymentID_ELV']);
+        }
+
+        return false;
     }
     
     public function hasCcPaymentId($userId)
     {
-        $data = $this->loadFastCheckoutData($userId);
-        
-        return $data && array_key_exists('paymentID_CC', $data) && !empty($data['paymentID_CC']);
+        if(isset($userId) && $userId != ''){
+            $data = $this->loadFastCheckoutData($userId);
+            return $data && array_key_exists('paymentID_CC', $data) && !empty($data['paymentID_CC']);
+        }
+
+        return false;
     }
 
     public function setFastCheckoutFlag($fastCheckoutFlag)
